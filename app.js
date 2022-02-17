4d6b6b265d9572aebca7018b75f4b9df9b5023e7
coszach@hotmail.it <coszach@hotmail.it>
2022-02-17 16:59:28 +0100

    stampata la nuova card

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'img/wayne-barnett-founder-ceo.jpg',
+    image: 'wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'img/angela-caroll-chief-editor.jpg',
+    image: 'angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'img/walter-gordon-office-manager.jpg',
+    image: 'walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'img/angela-lopez-social-media-manager.jpg',
+    image: 'angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'img/scott-estrada-developer.jpg',
+    image: 'scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'img/barbara-ramos-graphic-designer.jpg',
+    image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
 
@@ -40,20 +40,36 @@ function createNewMember (){
     image : document.getElementById('image').value,
   }
   team.push(newMember);
-
+  const name = team[team.length-1].name ;
+  const role = team[team.length-1].role ;
+  const image = team[team.length-1].image ;
+  const newMemberCard = `
+    <div class="team-card">
+      <div class="card-image">
+        <img
+          src="img/${image}"
+          alt="${name}"
+        />
+      </div>
+      <div class="card-text">
+        <h3>${name}</h3>
+        <p>${role}</p>
+      </div>
+  </div>
+  `
+  teamContainer.innerHTML += newMemberCard
 }
 
 for (let i = 0; i < team.length; i++){
   const name = team[i].name ;
   const role = team[i].role ;
   const image = team[i].image ;
-  // console.log(name, role, image)
   
   const memberCard = `
     <div class="team-card">
       <div class="card-image">
         <img
-          src="${image}"
+          src="img/${image}"
           alt="${name}"
         />
       </div>
@@ -65,6 +81,7 @@ for (let i = 0; i < team.length; i++){
   `
   teamContainer.innerHTML += memberCard
 }
+
 
 addMemberButton.addEventListener('click', createNewMember)
 
